dfe5078564b3a2bc0015f0c439b1292dfd20fafb
c0py <jsamin3@gmail.com>
2024-02-28 19:06:00 +0100

    10/46

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -1,10 +1,12 @@
+//spaghetti code xd
 class Bob {
   String response(String msg) {
     final questionMark = '?';
     int lastchar = msg.trim().length - 1;
-    if (msg.trim().isEmpty) 
-      return "Fine. Be that way!";
-    if (msg.toUpperCase() == msg && msg[lastchar] == questionMark && msg.contains(RegExp(r'[a-zA-Z]')))
+    if (msg.trim().isEmpty) return "Fine. Be that way!";
+    if (msg.toUpperCase() == msg &&
+        msg[lastchar] == questionMark &&
+        msg.contains(RegExp(r'[a-zA-Z]')))
       return "Calm down, I know what I\'m doing!";
     else if (msg[lastchar] == questionMark)
       return "Sure.";
